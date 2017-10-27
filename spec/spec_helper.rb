c8762b8208609e4e3cc21df031e2229b8252f2c7
artemis <artemisxen@gmail.com>
2017-10-27 02:01:22 +0100

    setup project, generate gemfile and rspec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,13 @@
+require 'simplecov'
+require 'simplecov-console'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::Console
+
+])
+SimpleCov.start
+
+RSpec.configure do |config|
+  config.after(:suite) do
+  end
+end
